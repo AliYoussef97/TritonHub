d623a615934d271531b4792397ff6debac035d5c
Ali Youssef <ali97_mohamed@hotmail.com>
2024-10-15 02:22:12 +0300

    Add Tanh activation function

--- TritonHub/Activations/__init__.py
+++ TritonHub/Activations/__init__.py
@@ -4,5 +4,6 @@ from .relu import ReLU
 from .leaky_relu import LeakyReLU
 from .softmax import Softmax
 from .softplus import Softplus
+from .tanh import Tanh
 
-__all__ = ['GeLU', 'SiLU', 'ReLU', 'LeakyReLU', 'Softmax', 'Softplus']
+__all__ = ['GeLU', 'SiLU', 'ReLU', 'LeakyReLU', 'Softmax', 'Softplus', 'Tanh']
---
+++ TritonHub/Activations/tanh.py
@@ -0,0 +1,111 @@
+import torch
+import triton
+import triton.language as tl
+
+@triton.autotune(
+    configs=[
+        triton.Config({}, num_warps=2),
+        triton.Config({}, num_warps=4),
+        triton.Config({}, num_warps=8),
+        triton.Config({}, num_warps=16),
+        triton.Config({}, num_warps=32)
+    ],
+    key=['N'],
+)
+@triton.jit
+def _tanh_kernel_fwd(X, stride_X_row,
+                     Y, stride_Y_row,
+                     N, BLOCK_SIZE: tl.constexpr):
+    row = tl.program_id(0)
+    cols = tl.program_id(1) * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
+    X = X + row * stride_X_row
+    Y = Y + row * stride_Y_row
+    x = tl.load(X + cols, mask=cols < N, other=0.0).to(tl.float32)
+    exp_x = tl.exp(x)
+    n_exp_x = tl.exp(-x)
+    y = (exp_x - n_exp_x) / (exp_x + n_exp_x)
+    tl.store(Y + cols, y, mask=cols < N)
+
+def _tanh_fwd(x):
+    if x.stride(-1) != 1:
+        x = x.contiguous()
+    batch_shape = x.shape[:-1]
+    x = x.reshape(-1, x.shape[-1])
+    out = torch.empty_like(x, memory_format=torch.contiguous_format)
+    assert out.shape == x.shape, 'expect output shape to be the same as input shape'
+    assert out.stride(-1) == 1, 'expect output to be row-major'
+    M, N = x.shape
+    grid = lambda META: (M, triton.cdiv(N, META['BLOCK_SIZE']))
+    MAX_FUSED_SIZE = 65536 // x.element_size()
+    BLOCK_SIZE = min(MAX_FUSED_SIZE, triton.next_power_of_2(N))
+    with torch.cuda.device(x.device.index):
+        _tanh_kernel_fwd[grid](x, x.stride(0),
+                               out, out.stride(0),
+                               N, BLOCK_SIZE=BLOCK_SIZE)
+    return out.reshape(*batch_shape, out.shape[-1])
+
+@triton.autotune(
+    configs=[
+        triton.Config({}, num_warps=2),
+        triton.Config({}, num_warps=4),
+        triton.Config({}, num_warps=8),
+        triton.Config({}, num_warps=16),
+        triton.Config({}, num_warps=32)
+    ],
+    key=['N'],
+)
+@triton.jit
+def _tanh_kernel_bwd(X, stride_X_row,
+                     DOUT, stride_DOUT_row,
+                     DX, stride_DX_row,
+                     N, BLOCK_SIZE: tl.constexpr):
+    row = tl.program_id(0)
+    cols = tl.program_id(1) * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
+    X = X + row * stride_X_row
+    DOUT = DOUT + row * stride_DOUT_row
+    DX = DX + row * stride_DX_row
+    x = tl.load(X + cols, mask=cols < N, other=0.0).to(tl.float32)
+    dout = tl.load(DOUT + cols, mask=cols < N, other=0.0).to(tl.float32)
+    dx = dout * (1.0 - x * x)
+    tl.store(DX + cols, dx, mask=cols < N)
+
+def _tanh_bwd(x, dout):
+    if x.stride(-1) != 1:
+        x = x.contiguous()
+    if dout.stride(-1) != 1:
+        dout = dout.contiguous()
+    batch_shape = x.shape[:-1]
+    x = x.reshape(-1, x.shape[-1])
+    dout = dout.reshape(-1, dout.shape[-1])
+    assert x.shape == dout.shape, 'expect input and output shape to be the same'
+    dx = torch.empty_like(x, memory_format=torch.contiguous_format)
+    assert dx.stride(-1) == 1, 'expect derivative to be row-major'
+    M, N = x.shape
+    grid = lambda META: (M, triton.cdiv(N, META['BLOCK_SIZE']))
+    MAX_FUSED_SIZE = 65536 // x.element_size()
+    BLOCK_SIZE = min(MAX_FUSED_SIZE, triton.next_power_of_2(N))
+    with torch.cuda.device(x.device.index):
+        _tanh_kernel_bwd[grid](x, x.stride(0),
+                               dout, dout.stride(0),
+                               dx, dx.stride(0),
+                               N, BLOCK_SIZE=BLOCK_SIZE)
+    return dx.reshape(*batch_shape, dx.shape[-1])
+
+class tanh(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, input):
+        output = _tanh_fwd(input)
+        ctx.save_for_backward(output)
+        return output
+
+    @staticmethod
+    def backward(ctx, d_out):
+        output, = ctx.saved_tensors
+        grad = _tanh_bwd(output, d_out)
+        return grad
+
+class Tanh:
+    def __init__(self):
+        self.tanh_fn = tanh.apply
+    def __call__(self, x):
+        return self.tanh_fn(x)
