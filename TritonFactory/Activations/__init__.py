787e7890d26eed8e06965a47dee73b302717f641
Ali Youssef <ali97_mohamed@hotmail.com>
2024-10-14 15:00:17 +0300

    Softplus activation function

--- TritonFactory/Activations/__init__.py
+++ TritonFactory/Activations/__init__.py
@@ -3,5 +3,6 @@ from .silu import SiLU
 from .relu import ReLU
 from .leaky_relu import LeakyReLU
 from .softmax import Softmax
+from .softplus import Softplus
 
-__all__ = ['GeLU', 'SiLU', 'ReLU', 'LeakyReLU', 'Softmax']
+__all__ = ['GeLU', 'SiLU', 'ReLU', 'LeakyReLU', 'Softmax', 'Softplus']
